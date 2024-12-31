ea2d787f5e1c28d62bd2ce8a3be6a2c4d5456bca
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-12-31 23:38:26 +0530

    updated patient form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -11,6 +11,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/router";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType{
   INPUT='input',
@@ -43,10 +44,10 @@ const PatientForm = () => {
   async function onSubmit({name,email,phone}: z.infer<typeof UserFormValidation>) {
     setisLoading(true);
     try {
-     // const userData={name,email,phone };
-     //const user =  await createUser(userData);
+      const userData={name,email,phone };
+     const user =  await createUser(userData);
 
-     // if(user) router.push(`/patients/${user.$id}/register`)
+      if(user) router.push(`/patients/${user.$id}/register`)
     } catch (error) {
       console.log(error)
     }
