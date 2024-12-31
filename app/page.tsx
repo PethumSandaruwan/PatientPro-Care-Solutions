2c637cfe5630d8bbfe9c77c1500cb54718f27676
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-12-31 23:55:02 +0530

    updated homepage photo

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         </div>
       </section>
       <Image 
-     src="/assets/images/onboarding-img.png"
+     src="/assets/images/homepage.jpeg"
      height={1000}
      width={1000}
      alt="patient"
